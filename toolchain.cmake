b183e3f283769fd6570d4065bc856b561b70c660
Sysprogs <sysprogs@sysprogs.com>
2019-10-08 11:53:35 -0700

    Removed hardcoded directories and updated the toolchain reference

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,3 +1,7 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 SET(CMAKE_C_COMPILER "${TOOLCHAIN_ROOT}/bin/gcc.exe")
 SET(CMAKE_CXX_COMPILER "${TOOLCHAIN_ROOT}/bin/g++.exe")
+
+if(EXISTS "$ENV{TOOLCHAIN_ROOT}/Qt/v5-CMake/Qt5Cross.cmake")
+	include("$ENV{TOOLCHAIN_ROOT}/Qt/v5-CMake/Qt5Cross.cmake")
+endif()
