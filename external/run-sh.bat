9eebee346368cbc3002c7d36872a2a6d4607b2da
Sysprogs <sysprogs@sysprogs.com>
2019-11-17 19:32:02 -0800

    Added a better error message for missing sh.exe

--- external/run-sh.bat
+++ external/run-sh.bat
@@ -1,2 +1,2 @@
-set PATH=%PATH%;C:\MinGW\msys\1.0\bin
+@where sh || echo run-sh.bat:1: error : Could not find sh.exe. Please add the location of sh.exe from your Git installation to the PATH variable or modify run-sh.bat to use an explicit path.
 sh %1 %2 %3 %4 %5 %6 %7 %8 %9 %10
